864c6535a2634056ea017dbbdc7bb61a73a8db91
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2025-01-08 17:16:10 +0600

    some code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,125 +1,100 @@
-
-
-import './App.css'
+import "./App.css";
 import { useEffect, useState } from "react";
-import Banner from './components/Banner'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import Players from './components/Players'
-import { ToastContainer, toast } from 'react-toastify';
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Players from "./components/Players";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const notify = () => {
     toast.error("Not enough money to buy this player.Claim some Credit", {
       position: "top-center",
-      
     });
   };
   const notifyExistedPlayer = () => {
     toast.error(`Player already selected`, {
-      position: "top-center"
+      position: "top-center",
     });
   };
   const notifyChosenPlayer = (name) => {
     toast.success(`Congrates!! ${name} is now in your squad`, {
       position: "top-center",
-      autoClose:2000,
+      autoClose: 2000,
     });
   };
   const notifyClaim = () => {
     toast.success(`Credit Added to your Account`, {
       position: "top-center",
-      autoClose:2000,
+      autoClose: 2000,
     });
   };
   const notifyMaximizeTeam = () => {
     toast.error(`You cannot choose more than 6 player`, {
       position: "top-center",
-      autoClose:2000,
+      autoClose: 2000,
     });
   };
   const notifyPlayerRemoved = () => {
     toast.warn(`Player Removed`, {
       position: "top-right",
-      autoClose:2000,
+      autoClose: 2000,
     });
   };
   useEffect(() => {
-    fetch('players.json')
-      .then(res => res.json())
-      .then(data => setPlayerProfile(data))
-  }, [])
+    fetch("players.json")
+      .then((res) => res.json())
+      .then((data) => setPlayerProfile(data));
+  }, []);
 
-  const [playerProfile, setPlayerProfile] = useState([])
+  const [playerProfile, setPlayerProfile] = useState([]);
 
-  const [toggleStatus, setToggleStatus] = useState(true)
+  const [toggleStatus, setToggleStatus] = useState(true);
 
-  const [selectedPlayer, setSelectedPlayer] = useState([])
+  const [selectedPlayer, setSelectedPlayer] = useState([]);
 
-  const [amount, setAmount] = useState(0)
-
-
-
-
-
-
+  const [amount, setAmount] = useState(0);
 
   const handleToggle = (status) => {
-    setToggleStatus(status)
-  }
+    setToggleStatus(status);
+  };
 
   const handleChosenPlayer = (player, amountMinusBiddingPrice) => {
-
-    const sameItem = selectedPlayer.find((item) => player.id === item.id)
+    const sameItem = selectedPlayer.find((item) => player.id === item.id);
 
     if (sameItem) {
-      notifyExistedPlayer()
-    }
-    else if (amountMinusBiddingPrice < 0) {
-      notify()
-      return
-    }
-    else if (selectedPlayer.length > 5){
-      notifyMaximizeTeam()
-      return
-    }
-    else {
-     
+      notifyExistedPlayer();
+    } else if (amountMinusBiddingPrice < 0) {
+      notify();
+      return;
+    } else if (selectedPlayer.length > 5) {
+      notifyMaximizeTeam();
+      return;
+    } else {
       setAmount(amountMinusBiddingPrice);
-      setSelectedPlayer([...selectedPlayer, player])
-      notifyChosenPlayer(player.name)
+      setSelectedPlayer([...selectedPlayer, player]);
+      notifyChosenPlayer(player.name);
     }
-
-
-
-  }
-
+  };
 
   const handleRemove = (id, biddingPrice) => {
-    const remaining = selectedPlayer.filter((item) => id !== item.id)
-    setSelectedPlayer(remaining)
-    setAmount(amount + biddingPrice)
+    const remaining = selectedPlayer.filter((item) => id !== item.id);
+    setSelectedPlayer(remaining);
+    setAmount(amount + biddingPrice);
     notifyPlayerRemoved();
-  }
-
+  };
 
   const handleClaimBtn = () => {
-    setAmount(amount + 10000000)
-    notifyClaim()
-
-  }
-
+    setAmount(amount + 10000000);
+    notifyClaim();
+  };
 
   return (
     <>
-        <Navbar
-          amount={amount}
-        ></Navbar>
-      <div className='w-[90%] mx-auto'>
-        <Banner
-          handleClaimBtn={handleClaimBtn}
-        ></Banner>
+      <Navbar amount={amount}></Navbar>
+      <div className="w-[90%] mx-auto">
+        <Banner handleClaimBtn={handleClaimBtn}></Banner>
         <Players
           playerProfile={playerProfile}
           toggleStatus={toggleStatus}
@@ -132,9 +107,8 @@ function App() {
       </div>
       <Footer></Footer>
       <ToastContainer />
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
